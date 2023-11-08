b60071f762ef0c6a7176eca42573cec891c404ee
Marc Carlson <138807964+carls0n@users.noreply.github.com>
2023-11-08 10:11:26 -0800

    alpha2 now supports SSH attacks!

--- alpha2.sh
+++ alpha2.sh
@@ -2,6 +2,7 @@
 
 # alpha2.sh
 # written by Marc Carlson November 8, 2023
+# my other repositories: https://github.com/carls0n/
 # bash parallel SSH/HTTP bruteforcer. Tries all possible alphanumeric combinations based on the number of characters.
 # also has an option to use a password file instead.
 # this is free software. free to use, free to redistribute. Do whatever you like with it.
